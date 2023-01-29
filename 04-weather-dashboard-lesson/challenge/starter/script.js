12edc285f4802b07f565bbb851a46f194def12a7
Lawzstopwar <lawzstopwar@gmail.com>
2023-01-29 18:18:04 +0000

    added bootstrap to index html

--- 04-weather-dashboard-lesson/challenge/starter/script.js
+++ 04-weather-dashboard-lesson/challenge/starter/script.js
@@ -2,12 +2,15 @@
 
 fetch("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=0e1d4f15c175dd06881d40b79606df7b")
 .then(response => response.json())
-.then(data => {
+.then(citiesFound => {
+    let firstCity = citiesFound[0]
+    console.log(firstCity.lat);
+    console.log(firstCity.lon);
 
-    console.log(data);
-
+    return fetch (`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&Lon=${firstCity.lon}&appid=0e1d4f15c175dd06881d40b79606df7b`)
 })
-fetch ("https://api.openweathermap.org/data/2.5/forecast?lat=44&lon=10.99&appid=0e1d4f15c175dd06881d40b79606df7b")
+
+
 .then(response => response.json())
 .then(data => {
 
